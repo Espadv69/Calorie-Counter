5aa3b18404d61449402f1ec607eba38212f9656d
Espadv69 <aitorjob69@gmail.com>
2025-02-24 12:06:45 +0100

    Add state management for food name and calories in FoodEntry component

--- src/components/FoodEntry/FoodEntry.jsx
+++ src/components/FoodEntry/FoodEntry.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
 import './FoodEntry.css'
 
-const FoodEntry = ({ onAddFood }) => {}
+const FoodEntry = ({ onAddFood }) => {
+  const [foodName, setFoodName] = useState('') // State to handle input value
+  const [calories, setCalories] = useState('') // State to handle input value
+}
 
 export default FoodEntry
