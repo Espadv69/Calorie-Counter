76693070220daac3b9dbb5c9b17c9411b9a74097
Espadv69 <aitorjob69@gmail.com>
2025-02-24 12:16:26 +0100

    Add food list display and handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,25 @@ import FoodEntry from './components/FoodEntry/FoodEntry'
 const App = () => {
   const [foods, setFoods] = useState([]) // State to store food data
 
+  const handleAddFood = (newFood) => {
+    setFoods([...foods, newFood]) // Update food data
+  }
+
   return (
     <div>
       <Header />
+      <FoodEntry onAddFood={handleAddFood} />
+
+      <div className="food-container">
+        <h2>Food List</h2>
+        <ul>
+          {foods.map((food, index) => (
+            <li key={index}>
+              {food.foodName} - {food.calories} calories
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
