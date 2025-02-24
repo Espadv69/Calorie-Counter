0f23ee2cea8145202a6d8f4487e8d438cfcf93a2
Espadv69 <aitorjob69@gmail.com>
2025-02-24 11:35:05 +0100

    Fix App component return statement syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header'
 
 const App = () => {
-  ;<div>
-    <Header />
-  </div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App
