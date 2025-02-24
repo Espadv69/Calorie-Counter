e9bf402e234d0d2b33bd438d9e68975030991016
Espadv69 <aitorjob69@gmail.com>
2025-02-24 12:13:33 +0100

    Add state management for food data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
+
 import Header from './components/Header/Header'
+import FoodEntry from './components/FoodEntry/FoodEntry'
 
 const App = () => {
+  const [foods, setFoods] = useState([]) // State to store food data
+
   return (
     <div>
       <Header />
