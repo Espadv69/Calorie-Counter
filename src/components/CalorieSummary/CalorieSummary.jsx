8348726b28a6b3a5601cb4fa90ee5e332aae06ef
Espadv69 <aitorjob69@gmail.com>
2025-02-28 19:32:26 +0100

    Display total calories consumed in CalorieSummary component

--- src/components/CalorieSummary/CalorieSummary.jsx
+++ src/components/CalorieSummary/CalorieSummary.jsx
@@ -1,5 +1,12 @@
 const CalorieSummary = ({ foods }) => {
   const totalCalories = foods.reduce((sum, food) => sum + food.calories, 0)
+
+  return (
+    <div>
+      <h2>Calorie Summary</h2>
+      <p>Total Calories Consumed: {totalCalories}</p>
+    </div>
+  )
 }
 
 export default CalorieSummary
