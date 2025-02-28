0da23cebb82913f99483dc2dfb5006abe1b87c20
Espadv69 <aitorjob69@gmail.com>
2025-02-28 19:33:26 +0100

    Add CalorieSummary component to App for displaying food data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header/Header'
 import FoodEntry from './components/FoodEntry/FoodEntry'
+import CalorieSummary from './components/CalorieSummary/CalorieSummary'
 
 const App = () => {
   const [foods, setFoods] = useState([]) // State to store food data
@@ -14,6 +15,7 @@ const App = () => {
     <div>
       <Header />
       <FoodEntry onAddFood={handleAddFood} />
+      <CalorieSummary foods={foods} />
 
       <div className="food-container">
         <h2>Food List</h2>
