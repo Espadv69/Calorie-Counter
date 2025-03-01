d4ad562773ec472cd97402c3355a30d059ce06c2
Espadv69 <aitorjob69@gmail.com>
2025-03-01 11:57:32 +0100

    Add totalCalories prop and status message to GoalSetter component

--- src/components/GoalSetter/GoalSetter.jsx
+++ src/components/GoalSetter/GoalSetter.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
 
-const GoalSetter = () => {
+const GoalSetter = ({ totalCalories }) => {
   const [goal, setGoal] = useState(2000)
 
   const handleGoalChange = (e) => {
     setGoal(Number(e.target.value))
+  }
+
+  const getTotalStatus = () => {
+    if (totalCalories < goal) {
+      return `You have ${
+        goal - totalCalories
+      } calories left to reach your goal.`
+    } else if (totalCalories === goal) {
+      return 'You have reached your daily calorie goal!'
+    } else {
+      return `You have exceeded your goal by ${totalCalories - goal} calories.`
+    }
   }
 }
 
