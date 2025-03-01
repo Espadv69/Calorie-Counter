f14857e1decb0fa02a9f1f43638234ba87bb6a2c
Espadv69 <aitorjob69@gmail.com>
2025-03-01 15:07:22 +0100

    Add auto-dismiss functionality to Notification component

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,16 @@
 import { useEffect } from 'react'
 
-const Notification = ({ message, onClose }) => {}
+const Notification = ({ message, onClose }) => {
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        onClose()
+      }, 3000)
+      return () => clearTimeout(timer)
+    }
+  }, [message, onClose])
+
+  if (!message) return null
+}
 
 export default Notification
