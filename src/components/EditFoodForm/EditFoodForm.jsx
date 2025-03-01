6ec850acf595e6df0f6cfa072d04f964e7156af4
Espadv69 <aitorjob69@gmail.com>
2025-03-01 12:23:35 +0100

    Add state management for food name and calories in EditFoodForm component

--- src/components/EditFoodForm/EditFoodForm.jsx
+++ src/components/EditFoodForm/EditFoodForm.jsx
@@ -1,3 +1,8 @@
-const EditFoodForm = () => {}
+import { useState } from 'react'
+
+const EditFoodForm = () => {
+  const [foodName, setFoodName] = useState('')
+  const [calories, setCalories] = useState('')
+}
 
 export default EditFoodForm
