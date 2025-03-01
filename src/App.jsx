aba1d05bfa9244bfe3acaecee090016120bcd8e5
Espadv69 <aitorjob69@gmail.com>
2025-03-01 12:02:50 +0100

    Add GoalSetter component to App and adjust food list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Header/Header'
 import FoodEntry from './components/FoodEntry/FoodEntry'
 import CalorieSummary from './components/CalorieSummary/CalorieSummary'
+import GoalSetter from './components/GoalSetter/GoalSetter'
 
 const App = () => {
   const [foods, setFoods] = useState([]) // State to store food data
@@ -17,20 +18,20 @@ const App = () => {
       <FoodEntry onAddFood={handleAddFood} />
       <CalorieSummary foods={foods} />
 
-    {foods.length !== 0 ? (
-      <div className="food-container">
-        <h2>Food List</h2>
-        <ul>
-          {foods.map((food, index) => (
-            <li key={index}>
-              {food.foodName} - {food.calories} calories
-            </li>
-          ))}
-        </ul>
-      </div>
-    ) : (
-      <p>No foods!</p>
-    )}
+      {foods.length !== 0 ? (
+        <div className="food-container">
+          <h2>Food List</h2>
+          <ul>
+            {foods.map((food, index) => (
+              <li key={index}>
+                {food.foodName} - {food.calories} calories
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No foods!</p>
+      )}
     </div>
   )
 }
