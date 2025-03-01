3b6ced4cbd79572e92cba304527e96f7543535ca
Espadv69 <aitorjob69@gmail.com>
2025-03-01 11:53:41 +0100

    Add goal change handler to GoalSetter component

--- src/components/GoalSetter/GoalSetter.jsx
+++ src/components/GoalSetter/GoalSetter.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 
 const GoalSetter = () => {
   const [goal, setGoal] = useState(2000)
+
+  const handleGoalChange = (e) => {
+    setGoal(Number(e.target.value))
+  }
 }
 
 export default GoalSetter
