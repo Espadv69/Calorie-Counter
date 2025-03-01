089d6b7b8bbbed2ff4b63a3f79d031a583bffbaa
Espadv69 <aitorjob69@gmail.com>
2025-03-01 15:12:37 +0100

    Add notification for added food item

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ const App = () => {
 
   const handleAddFood = (newFood) => {
     setFoods([...foods, newFood])
+    showNotification(`Added: ${newFood.foodName}`)
   }
 
   const handleDeleteFood = (index) => {
